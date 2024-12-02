f10bd82288bba582f7480c24ec8ff272936de290
Eric-Leal <ericgleal@gmail.com>
2024-12-02 18:38:01 -0300

    Update homepage

---
+++ public/assets/js/homepage.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', () => {
+    carouselSeries();
+    novasSeries();
+});
+
+const URL = 'https://api.themoviedb.org/3/';
+
+function carouselSeries() {
+    fetch(`${URL}/trending/tv/week?language=pt-BR&page=1`, {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmM0NjI0OTVkYTA1Njc2ZDdjMjJlYzUxMGRjODg3ZCIsIm5iZiI6MTczMDQ5MTA5OS41MTYsInN1YiI6IjY3MjUzMmRiOTAwMDQ4MTBmZWNiNDAwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gaPsIGk-oOxaKr9rgy2tcMQUNjsoJ09oq3KnVQMtREg'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            let str = '';
+            for (let i = 0; i < 4; i++) {
+                let serie = data.results[i];
+                str += `
+                    <div class="carousel-item ${i === 0 ? 'active' : ''}">
+                        <img src="https://image.tmdb.org/t/p/w500${serie.backdrop_path}" class="d-block w-100" alt="${serie.name}">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5 class="carouseldesc">
+                            <a href="pages/serie.html?id=${serie.id}">${serie.name}</a>
+                            </h5>
+                        </div>
+                    </div>
+                `;
+            }
+            document.getElementById('telaCarousel').innerHTML = str;
+        })
+        .catch(error => {
+            console.error('Erro ao buscar as séries:', error);
+        });
+}
+
+function novasSeries() {
+    fetch(`${URL}/discover/tv?language=pt-BR&primary_release_date.gte=2024-10-01&page=1`, {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmM0NjI0OTVkYTA1Njc2ZDdjMjJlYzUxMGRjODg3ZCIsIm5iZiI6MTczMDQ5MTA5OS41MTYsInN1YiI6IjY3MjUzMmRiOTAwMDQ4MTBmZWNiNDAwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gaPsIGk-oOxaKr9rgy2tcMQUNjsoJ09oq3KnVQMtREg'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            let str = '';
+            for (let i = 0; i < 4; i++) {
+                let serie = data.results[i];
+                
+                fetch(`${URL}/tv/${serie.id}/watch/providers?language=pt-BR`, {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmM0NjI0OTVkYTA1Njc2ZDdjMjJlYzUxMGRjODg3ZCIsIm5iZiI6MTczMDQ5MTA5OS41MTYsInN1YiI6IjY3MjUzMmRiOTAwMDQ4MTBmZWNiNDAwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gaPsIGk-oOxaKr9rgy2tcMQUNjsoJ09oq3KnVQMtREg'
+                    }
+                })
+                .then(res => res.json())
+                .then(providers => {
+                    let platforma = 'Desconhecido';
+                    if (providers.results) {
+                        if (providers.results['BR'] && providers.results['BR'].flatrate) {
+                            const platformas = providers.results['BR'].flatrate;
+                            platforma = platformas.length > 0 ? platformas[0].provider_name : 'Desconhecido';
+                        }
+                    }
+    
+                    str += `
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card mb-3">
+                                <img src="https://image.tmdb.org/t/p/w1280${serie.backdrop_path}" class="card-img-top" alt="${serie.name}">
+                                <div class="card-body">
+                                    <h5 class="card-title">${serie.name}</h5>
+                                    <p class="card-text mb-0">${serie.first_air_date}</p>
+                                    <p class="card-text mb-2">${platforma}</p> <!-- Aqui você exibe a plataforma -->
+                                    <a href="../pages/serie.html?id=${serie.id}" class="btn btn-primary">Ver mais</a>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+
+                    document.getElementById('telaNovasSeries').innerHTML = str;
+                })
+                .catch(error => {
+                    console.error('Erro ao buscar plataformas:', error);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao buscar as séries:', error);
+        });
+}
---
+++ public/assets/js/serie.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const parametros = new URLSearchParams(window.location.search);
+    const idSerie = parametros.get('id');
+    if (idSerie) {
+        carregarDadosSerie(idSerie);
+    } else {
+        console.error('ID da série não encontrado');
+    }
+});
+
+function carregarDadosSerie(id) {
+    const URL = `https://api.themoviedb.org/3/tv/${id}?api_key=3bc462495da05676d7c22ec510dc887d&language=pt-BR`;
+
+    fetch(URL)
+        .then(resposta => resposta.json())
+        .then(dados => {
+
+            document.querySelector('#nomeSerie').innerText = dados.name;
+            document.querySelector('#imagemSerie').src = `https://image.tmdb.org/t/p/w500${dados.poster_path}`;
+            document.querySelector('#genero').innerHTML = `<span class="destaque">Gênero:</span> ${dados.genres.map(genero => genero.name).join(', ')}`;
+            document.querySelector('#dataLancamento').innerHTML = `<span class="destaque">Data de Lançamento:</span> ${dados.first_air_date}`;
+            document.querySelector('#sinopse').innerHTML = `<span class="destaque">Sinopse:</span> ${dados.overview}`;
+            document.querySelector('#plataforma').innerHTML = `<span class="destaque">Plataforma:</span> ${dados.networks.map(rede => rede.name).join(', ')}`;
+            document.querySelector('#temporadas').innerHTML = `<span class="destaque">Temporadas:</span> ${dados.number_of_seasons}`;
+
+            carregarElenco(id);
+        })
+        .catch(erro => {
+            console.error('Erro ao buscar os dados da série:', erro);
+        });
+}
+
+function carregarElenco(id) {
+    const urlElenco = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=3bc462495da05676d7c22ec510dc887d&language=pt-BR`;
+
+    fetch(urlElenco)
+        .then(resposta => resposta.json())
+        .then(dadosElenco => {
+            const listaDeAtores = dadosElenco.cast;
+            const tamanhoDoGrupo = 4;
+            let contadorDeSlides = 0;
+
+            for (let i = 0; i < listaDeAtores.length; i += tamanhoDoGrupo) {
+                let htmlDoSlide = `<div class="carousel-item ${contadorDeSlides === 0 ? 'active' : ''}">`;
+                contadorDeSlides++; 
+
+                htmlDoSlide += `<div class="row text-center">`; 
+                for (let j = i; j < i + tamanhoDoGrupo && j < listaDeAtores.length; j++) {
+                    const ator = listaDeAtores[j];
+                    htmlDoSlide += `
+                        <div class="col-md-6 col-lg-3 mb-4">
+                            <div class="card">
+                                <img src="https://image.tmdb.org/t/p/w500${ator.profile_path}" class="card-img-top elenco-img" alt="${ator.name}">
+                                <div class="card-body">
+                                    <h4 class="card-text mb-0">${ator.name}</h4>
+                                    <p>${ator.character}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                }
+                htmlDoSlide += `</div>`; 
+                htmlDoSlide += `</div>`; 
+
+                document.getElementById('carouselActors').innerHTML += htmlDoSlide;
+            }
+        })
+        .catch(erro => {
+            console.error('Erro ao buscar o elenco da série:', erro);
+        });
+}
